24a45ff0ed8c570172e4e4e3b50ccdd1450cc0c1
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-07-07 16:16:56 +0700

    gitignore updated

--- server/database.sql
+++ server/database.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE postgres;
 
-CREATE TABLE cocktails (
+CREATE TABLE cocktail (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     createdBy VARCHAR(255),
+    -- createdTimeStamp 
     category VARCHAR(50),
     ingredients VARCHAR(50)[],
     instructions VARCHAR(255)[]
 );
-
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
