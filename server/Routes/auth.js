a05778e550cee8ee822c837a7c7619a56b079e99
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-07-14 13:00:32 +0700

    Quotations fix

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -53,7 +53,7 @@ export const ensureAuthenticated = (req, res, next) => {
         return next();
     }
     res.status(401).send("Unauthorised");
-    // res.redirect('/login');
+    // res.redirect("/login");
 }
 
 // Check user authentication status (frontend)
@@ -82,13 +82,13 @@ authRouter.get("/api/login/status", (req, res) => {
 })
 
 // Login user with passport
-// authRouter.post("/api/login", passport.authenticate("local", { failureRedirect: '/login', failureMessage: "true" }), (req, res) => {
+// authRouter.post("/api/login", passport.authenticate("local", { failureRedirect: "/login", failureMessage: "true" }), (req, res) => {
 //     console.log("--> Login Endpoint");
 //     return res.status(200).send(req.user);
 // })
 
 authRouter.post("/api/login", (req, res, next) => {
-    passport.authenticate('local', function (err, user, info) {
+    passport.authenticate("local", function (err, user, info) {
         if (err) { return next(err); }
         if (!user) { return res.status(401).json(info.message); }
         req.logIn(user, function (err) {
